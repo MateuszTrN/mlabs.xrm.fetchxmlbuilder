f4ca9284063a2edbdd789279a938f65711cb9053
MateuszTrN <mateusz.sidorski@gmail.com>
2016-12-12 09:29:50 +0100

    initial

---
+++ xrm.fetchxmlbuilder.js
@@ -0,0 +1,273 @@
+var mlabs = mlabs || {};
+mlabs.xrm = mlabs.xrm || {};
+
+mlabs.xrm.xmlStringBuilder = function () {
+    var _xml = "";
+    var _tagsStack = [];
+    var _openTag = function (tag, attributes) {
+        _xml += "<" + tag;
+
+        for (var attr in attributes) {
+            if (attributes.hasOwnProperty(attr) && attributes[attr] != null && typeof attributes[attr] !== "undefined") {
+                _xml += " " + attr + "=\"" + attributes[attr] + "\"";
+            }
+        }
+        _xml += ">";
+        _tagsStack.push("</" + tag + ">");
+    };
+
+    var _closeTag = function () {
+        _xml += _tagsStack.pop();
+    };
+
+    return {
+        getXmlString: function () {
+            if (_tagsStack.length > 0) {
+                console.log("Some of tags were not close. Please use '.closeAllTags()' or validate your building steps");
+            }
+
+            return _xml;
+        },
+        openTag: function (tag, attributes) {
+            _openTag(tag, attributes);
+            return this;
+        },
+        insertString: function (xmlString) {
+            _xml += xmlString;
+        },
+        closeTag: function () {
+            _closeTag();
+            return this;
+        },
+        closeAllTags: function () {
+            while (_tagsStack.length > 0) {
+                _closeTag();
+            }
+            return this;
+        }
+    };
+}
+
+mlabs.xrm.entityBuilder = function (rootElementName, rootElementAttributesNameArray) {
+    var _entityLinks = [];
+    var _attributes = [];
+    var _filters = [];
+    var _linkEntities = [];
+    var _allAttributesSelected = true;
+    var _rootElementName = rootElementName;
+    var _rootAttributes = {};
+    var _setRootAttribute = function (name) {
+        return function (value) {
+            _rootAttributes[name] = value;
+            return this;
+        }
+    };
+
+    rootElementAttributesNameArray = rootElementAttributesNameArray || []
+    for (var i = 0; i < rootElementAttributesNameArray.length; i++) {
+
+        var methodName = rootElementAttributesNameArray[i];
+        methodName = methodName.split('-').map(function (attrName) {
+            return attrName.charAt(0).toUpperCase() + attrName.slice(1);
+        }).join('');
+        this["set" + methodName] = _setRootAttribute(rootElementAttributesNameArray[i]);
+    }
+
+    var _conditionBuilder = function () {
+        var _values = [];
+        var _attributeName = "";
+        var _operator = "eq";
+
+        var operator = function (operatorName) {
+            return function () {
+                _operator = operatorName;
+                _values = arguments;
+
+            }
+        }
+
+        this.getConditionXml = function () {
+            var xmlBuilder = new mlabs.xrm.xmlStringBuilder();
+            var rootAttr = { attribute: _attributeName, operator: _operator };
+
+            if (_values.length == 1) {
+                rootAttr["value"] = _values[0];
+            }
+
+            xmlBuilder.openTag("condition", rootAttr);
+            if (_values.length > 1) {
+                for (var i = 0; i < _values.length; i++) {
+                    xmlBuilder.openTag("value");
+                    xmlBuilder.insertString(_values[i]);
+                    xmlBuilder.closeTag();
+                }
+            }
+            xmlBuilder.closeTag();
+
+            return xmlBuilder.getXmlString();
+        }
+
+        this.builderApi = {
+            attribute: function (attributeName) {
+                _attributeName = attributeName;
+                return {
+                    equals: operator("eq"),
+                    isNotEqual: operator("ne"),
+                    isLessThan: operator("lt"),
+                    isLessEqual: operator("le"),
+                    isGreaterThan: operator("gt"),
+                    isGreaterEqual: operator("ge"),
+                    contains: operator("like"),
+                    isIn: operator("in"),
+                    isBetween: operator("between"),
+                    doesNotContain: operator("not-like"),
+                    //todo: more operators if needed                
+                }
+            }
+        };
+    }
+
+    var _filterBuilder = function () {
+        var _conditions = [];
+        var _builderFilters = [];
+        var _type = "and";
+
+        this.builderApi = {
+            type: function (type) {
+                type = (type || "").toLowerCase();
+                if (type !== "or" && type !== "and") {
+                    throw new Error("FetchXml builder: Filter type can only have values: 'or', 'and'")
+                }
+                _type = type;
+                return this;
+            },
+            addFilter: function (filteringFunction) {
+                var builder = new _filterBuilder();
+                filteringFunction(builder.builderApi);
+                _builderFilters.push(builder.getFilterXml());
+                return this;
+            },
+            addCondition: function (conditionFunction) {
+                var conditionBuilder = new _conditionBuilder();
+                conditionFunction(conditionBuilder.builderApi);
+                _conditions.push(conditionBuilder.getConditionXml());
+                return this;
+            }
+        };
+
+        this.getFilterXml = function () {
+            var xmlBuilder = new mlabs.xrm.xmlStringBuilder();
+            xmlBuilder.openTag("filter", { "type": _type });
+            for (var i = 0; i < _conditions.length; i++) {
+                xmlBuilder.insertString(_conditions[i]);
+            }
+
+            for (var i = 0; i < _builderFilters.length; i++) {
+                xmlBuilder.insertString(_builderFilters[i]);
+            }
+
+            xmlBuilder.closeTag();
+
+            return xmlBuilder.getXmlString();
+        }
+    };
+
+    this.addFilter = function (filteringFunction) {
+        var builder = new _filterBuilder();
+        filteringFunction(builder.builderApi);
+        _filters.push(builder.getFilterXml())
+        return this;
+    }
+
+    this.setEntityName = function (entityName) {
+        _rootAttributes["name"] = entityName;
+        return this;
+    }
+
+    this.addAttribute = function (attributeName) {
+        _attributes.push(attributeName);
+        _allAttributesSelected = false;
+
+        return this;
+    }
+
+    this.addEntityLink = function (entityLinkBuildingFunction) {
+        var builder = new mlabs.xrm.entityBuilder("link-entity", ["from", "to", "name", "link-type"]);
+        entityLinkBuildingFunction(builder);
+        _entityLinks.push(builder.getEntityXml());
+        return this;
+    }
+
+    this.getEntityXml = function () {
+        var xmlBuilder = new mlabs.xrm.xmlStringBuilder();
+        xmlBuilder.openTag(_rootElementName, _rootAttributes);
+
+        if (_attributes.length && !_allAttributesSelected) {
+            for (var i = 0; i < _attributes.length; i++) {
+                xmlBuilder.openTag("attribute", { name: _attributes[i] });
+                xmlBuilder.closeTag();
+            }
+        } else {
+            xmlBuilder.openTag("all-attributes");
+            xmlBuilder.closeTag();
+        }
+
+        for (var i = 0; i < _filters.length; i++) {
+            xmlBuilder.insertString(_filters[i]);
+        }
+
+        for (var i = 0; i < _entityLinks.length; i++) {
+            xmlBuilder.insertString(_entityLinks[i]);
+        }
+
+        xmlBuilder.closeTag();
+
+        return xmlBuilder.getXmlString();
+    }
+
+}
+
+mlabs.xrm.queryBuilder = function () {
+    var _count;
+    var _top = -1;
+    var _distinct = true;
+
+    this.setCount = function (count) {
+        _count = count;
+        return this;
+    }
+
+    this.setTop = function (top) {
+        _top = top;
+        return this;
+    }
+
+    this.setDistinct = function (shouldDistinct) {
+        _distinct = shouldDistinct;
+    }
+
+    var _getFetchAttributes = function () {
+        var attributes = {};
+        attributes["count"] = _count || 5000;
+        attributes["distinct"] = _distinct || true;
+        if (_top > -1) {
+            attributes["top"] = _top;
+            delete attributes["count"];
+        }
+
+        return attributes;
+    }
+
+    mlabs.xrm.entityBuilder.call(this, "entity", ["name"]);
+
+    this.getFetchXml = function () {
+        var xmlBuilder = new mlabs.xrm.xmlStringBuilder();
+        xmlBuilder.openTag("fetch", _getFetchAttributes());
+        xmlBuilder.insertString(this.getEntityXml());
+        xmlBuilder.closeTag();
+
+        return xmlBuilder.getXmlString();
+    }
+}
+
+module.exports = mlabs;
---
+++ xrm.fetchxmlbuilder.spec.js
@@ -0,0 +1,137 @@
+var assert = require("assert");
+var formattor = require("formattor");
+xmlFormat = function (xml) {
+    return formattor(xml, { method: 'xml' });
+}
+
+var mlabs = require("./xrm.fetchxmlbuilder");
+
+describe("My very first javascript unit test ever ", function () {
+    it("The very first test should just work", function () {
+        assert.equal(1, 1);
+    });
+});
+
+describe("Xml Builder", function () {
+    it("should create simple XML without exception", function () {
+        assert.doesNotThrow(function () {
+            var builder = new mlabs.xrm.xmlStringBuilder();
+            builder.openTag("root");
+            builder.closeTag();
+        });
+    });
+});
+
+describe("Query Builder", function () {
+    it("doesn't throw an exception on complete usage example", function () {
+        try {
+            var query = new mlabs.xrm.queryBuilder();
+
+            query.setEntityName("account")
+                .addFilter(function (filter) {
+                    filter
+                        .type("or")
+                        .addCondition(function (where) {
+                            where.attribute("name").equals("Test");
+                        })
+                        .addCondition(function (where) {
+                            where.attribute("counter").isIn(1, 2, 3, 4, 5);
+                        })
+                        .addFilter(function (filter) {
+                            filter.type("and")
+                                .addCondition(function (where) {
+                                    where.attribute("type").equals("MyType");
+                                })
+                                .addCondition(function (where) {
+                                    where.attribute("statusreason").equals(0);
+                                });
+                        });
+                })
+                .addEntityLink(function (queryBuilder) {
+                    queryBuilder
+                        .setEntityName("contact")
+                        .setFrom("contactid")
+                        .setTo("parentaccountid")
+                        .setLinkType("outer")
+                        .addFilter(function (filter) {
+                            filter
+                                .type("or")
+                                .addCondition(function (where) {
+                                    where.attribute("id").equals("{00000000-0000-0000-0000-000000000000}");
+                                })
+                        });
+                });
+
+            var xml = xmlFormat(query.getFetchXml());
+
+            assert.ok(true);
+            console.log(xml);
+        } catch (err) {
+            assert.fail(err);
+        }
+    });
+
+    it("sets entity name correctly", function () {
+        var query = new mlabs.xrm.queryBuilder();
+        query.setEntityName("account");
+        var xml = xmlFormat(query.getFetchXml());
+
+        assert.ok(/\<entity\s+name\=('|\"(.*)\")\s*\>/i.test(xml));
+        console.log(xml);
+    });
+
+    it("should remove all-attributes element when addAttribute method is called", function () {
+        var query = new mlabs.xrm.queryBuilder();
+        query.setEntityName("account");
+        query.addAttribute("Name");
+        var xml = xmlFormat(query.getFetchXml());
+
+        assert.ok(!(/\<all-attributes(.*)\>/i.test(xml)));
+        console.log(xml);
+    });
+
+    it("should add filter element when addFilter method is called", function () {
+        var query = new mlabs.xrm.queryBuilder();
+        query.setEntityName("account");
+        query.addFilter(function (filter) {
+            filter.type("or").addCondition(function (where) {
+                where.attribute("name").equals("Test");
+            });
+        });
+
+        var xml = xmlFormat(query.getFetchXml());
+
+        assert.ok(/\<filter\s+type\=(\'|\")or(\'|\")(.*)\>\s+(\<condition(.*)\>)\s+\<\/filter\>/i.test(xml));
+        console.log(xml);
+    });
+
+    it("should add correct condition with single value", function () {
+        var query = new mlabs.xrm.queryBuilder();
+        query.setEntityName("account");
+        query.addFilter(function (filter) {
+            filter.addCondition(function (where) {
+                where.attribute("new_counter").equals(1);
+            });
+        });
+
+        var xml = xmlFormat(query.getFetchXml());
+
+        assert.ok(/(\<condition\s+(((attribute|operator|value)=[\"|\']\w+[\"|\'])\s?)+(\s?)+\>).*\<\/condition\>/igm.test(xml));
+        console.log(xml);
+    });
+
+    it("should add correct condition with multiple values", function () {
+        var query = new mlabs.xrm.queryBuilder();
+        query.setEntityName("account");
+        query.addFilter(function (filter) {
+            filter.addCondition(function (where) {
+                where.attribute("new_counter").isIn(1, 2);
+            });
+        });
+
+        var xml = xmlFormat(query.getFetchXml());
+
+        assert.ok(/(\<condition\s+(((attribute|operator)=[\"|\']\w+[\"|\'])\s?)+(\s?)+\>)(\s*(\<value\>\d\<\/value\>)\s*)*\<\/condition\>/igm.test(xml));
+        console.log(xml);
+    });
+});
